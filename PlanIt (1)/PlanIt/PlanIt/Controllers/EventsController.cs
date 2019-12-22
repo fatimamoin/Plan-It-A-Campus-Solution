8d417ff673842c3bff141b2c1aa6f71d2d7304b2
fm <fm03771@st.habib.edu.pk>
2019-12-22 19:40:41 +0500

    DONE

---
+++ PlanIt (1)/PlanIt/PlanIt/Controllers/EventsController.cs
@@ -0,0 +1,148 @@
+﻿using PlanIt.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlanIt.Models
+{
+    public class EventsController : Controller
+    {
+        public static int event_id = 0;
+        private Database1Entities db = new Database1Entities();
+
+        // GET: Events
+        [Authorize(Roles = "Club")]
+        public ActionResult Index()
+        {
+            if (db.Events.FirstOrDefault(c => c.Club_idClub == AccountController.user_id) != null)
+            {
+                event_id = db.Events.FirstOrDefault(c => c.Club_idClub == AccountController.user_id).idEvents;
+            }
+            var events = db.Events.Include(c => c.Club).Where(c => c.Club_idClub == AccountController.user_id);
+
+            //return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            var seat = Events_has_Member.seats(50);
+            ViewBag.seats = seat;
+            return View(events.ToList());
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        [Authorize(Roles = "Club")]
+        public ActionResult Create()
+        {
+            ViewBag.Club_idClub = db.Clubs.FirstOrDefault(x => x.idClub == AccountController.user_id).Name;
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idEvents,Club_idClub,Name,Date,Location,description, seats")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                @event.Club_idClub = AccountController.user_id;
+                @event.idEvents = db.Events.Max(u => u.idEvents) + 1;
+                db.Events.Add(@event);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Club_idClub = db.Clubs.FirstOrDefault(x => x.idClub == AccountController.user_id).Name;
+            return View(@event);
+        }
+
+        // GET: Events/Edit/5
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Club_idClub = db.Clubs.FirstOrDefault(x => x.idClub == AccountController.user_id).Name;
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idEvents,Club_idClub,Name,Date,Location,description,seats")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                @event.Club_idClub = AccountController.user_id;
+                db.Entry(@event).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Club_idClub = new SelectList(db.Clubs, "idClub", "Name", @event.Club_idClub);
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        [Authorize(Roles = "Club")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event @event = db.Events.Find(id);
+            db.Events.Remove(@event);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PlanIt (1)/PlanIt/PlanIt/Models/Events_has_MemberController.cs
@@ -0,0 +1,144 @@
+﻿using PlanIt.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlanIt.Models
+{
+    public class Events_has_MemberController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        [Authorize(Roles = "Club")]
+        // GET: Events_has_Member
+        public ActionResult Index()
+        {
+            var events_has_Member = db.Events_has_Member.Include(e => e.Club_member).Include(e => e.Event).Where(e=> e.Events_idEvents == EventsController.event_id);
+            return View(events_has_Member.ToList());
+        }
+
+        // GET: Events_has_Member/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Events_has_Member events_has_Member = db.Events_has_Member.Find(id);
+            if (events_has_Member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(events_has_Member);
+        }
+
+        // GET: Events_has_Member/Create
+        [Authorize(Roles = "Student")]
+        public ActionResult Create()
+        {
+            ViewBag.Club_member_idClub_members = db.Students.FirstOrDefault(x => x.idStudent == AccountController.user_id).Name;
+            ViewBag.Events_idEvents = new SelectList(db.Events, "idEvents", "Name");
+            return View();
+        }
+
+        // POST: Events_has_Member/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idEvents_has_Member,Club_member_idClub_members,Events_idEvents, Why")] Events_has_Member events_has_Member)
+        {
+            if (ModelState.IsValid)
+            {
+                events_has_Member.idEvents_has_Member = db.Events_has_Member.Max(u => u.idEvents_has_Member) + 1;
+                events_has_Member.Club_member_idClub_members = db.Club_member.FirstOrDefault(u => u.Student_idStudent
+                == AccountController.user_id).idClub_members;
+                db.Events_has_Member.Add(events_has_Member);
+                var old = db.Events.FirstOrDefault(x => x.idEvents == events_has_Member.Events_idEvents).seats;
+                int seat_new = Events_has_Member.seats(System.Convert.ToInt32(old));
+                db.Events.FirstOrDefault(x => x.idEvents == events_has_Member.Events_idEvents).seats = seat_new;
+                db.SaveChanges();
+                return RedirectToAction("Index","Students");
+            }
+            
+            ViewBag.Club_member_idClub_members = db.Students.FirstOrDefault(x => x.idStudent == AccountController.user_id).Name;
+            ViewBag.Events_idEvents = new SelectList(db.Events, "idEvents", "Name"); 
+            return View(events_has_Member);
+        }
+
+        // GET: Events_has_Member/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Events_has_Member events_has_Member = db.Events_has_Member.Find(id);
+            if (events_has_Member == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Club_member_idClub_members = new SelectList(db.Club_member, "idClub_members", "Student_idStudent", events_has_Member.Club_member_idClub_members);
+            ViewBag.Events_idEvents = new SelectList(db.Events, "idEvents", "Club_idClub", events_has_Member.Events_idEvents);
+            return View(events_has_Member);
+        }
+
+        // POST: Events_has_Member/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idEvents_has_Member,Club_member_idClub_members,Events_idEvents")] Events_has_Member events_has_Member)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(events_has_Member).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Club_member_idClub_members = new SelectList(db.Club_member, "idClub_members", "Student_idStudent", events_has_Member.Club_member_idClub_members);
+            ViewBag.Events_idEvents = new SelectList(db.Events, "idEvents", "Club_idClub", events_has_Member.Events_idEvents);
+            return View(events_has_Member);
+        }
+
+        // GET: Events_has_Member/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Events_has_Member events_has_Member = db.Events_has_Member.Find(id);
+            if (events_has_Member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(events_has_Member);
+        }
+
+        // POST: Events_has_Member/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Events_has_Member events_has_Member = db.Events_has_Member.Find(id);
+            db.Events_has_Member.Remove(events_has_Member);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
